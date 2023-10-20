fec216bdbf82448fc213806c7aab7c764d69f17c
AyhaamB <ayh132@gmail.com>
2023-10-20 14:10:32 -0400

    searchbar

--- client/src/componets/Header.jsx
+++ client/src/componets/Header.jsx
@@ -76,6 +76,19 @@ export default function Header() {
             </li> */}
           </ul>
         </div>
+        <div class="container-fluid">
+          <form class="d-flex" role="search">
+            <input
+              class="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            ></input>
+            <button class="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
       </div>
     </nav>
   );
