7199020ab4699e4b7805bd99a1b7b481ed2ad3bb
LeopoldoGurgel <leopoldogbp@gmail.com>
2023-10-20 11:33:06 -0400

    finished resolvers and added queries and mutations to type defs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,29 +1,203 @@
-const { Tech, Matchup } = require('../models');
+const { User, Sponsor, Product, Post } = require('../models');
 
 const resolvers = {
   Query: {
-    tech: async () => {
-      return Tech.find({});
+    users: async () => {
+      return User.find().populate('posts products');
     },
-    matchups: async (parent, { _id }) => {
-      const params = _id ? { _id } : {};
-      return Matchup.find(params);
+    user: async (parent, { name }) => {
+      return User.findOne({ name }).populate('posts products');
+    },
+    posts: async (parent, { author }) => {
+      const params = author ? { author } : {};
+      return Post.find(params).sort({ createdAt: -1 }).populate('comments');
+    },
+    post: async (parent, { postId }) => {
+      return Post.findOne({ _id: postId });
+    },
+    products: async (parent, { author }) => {
+      const params = author ? { author } : {};
+      return Product.find(params).sort({ createdAt: -1 }).populate('comments');
+    },
+    product: async (parent, { productId }) => {
+      return Product.findOne({ _id: productId });
+    },
+    sponsors: async (parent, { name }) => {
+      const params = name ? { name } : {};
+      return Sponsor.find(params).sort({ donation });
+    },
+    sponsor: async (parent, { name }) => {
+      return Sponsor.findOne({ name });
+    },
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id }).populate('posts products');
+      }
+      throw AuthenticationError;
     },
   },
+
   Mutation: {
-    createMatchup: async (parent, args) => {
-      const matchup = await Matchup.create(args);
-      return matchup;
+    addUser: async (parent, { name, email, password }) => {
+      const user = await User.create({ name, email, password });
+      const token = signToken(user);
+      return { token, user };
     },
-    createVote: async (parent, { _id, techNum }) => {
-      const vote = await Matchup.findOneAndUpdate(
-        { _id },
-        { $inc: { [`tech${techNum}_votes`]: 1 } },
-        { new: true }
-      );
-      return vote;
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw AuthenticationError;
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw AuthenticationError;
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
     },
-  },
+    addPost: async (parent, { content }, context) => {
+      if (context.user) {
+        const post = await Post.create({
+          content,
+          author: context.user.name,
+        });
+
+        await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { posts: post._id } }
+        );
+
+        return thought;
+      }
+      throw AuthenticationError;
+      ('You need to be logged in!');
+    },
+    addProduct: async (parent, { description, price, stock }, context) => {
+      if (context.user) {
+        const product = await Product.create({
+          description,
+          price, 
+          stock,
+          author: context.user.name,
+        });
+
+        await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { products: product._id } }
+        );
+
+        return thought;
+      }
+      throw AuthenticationError;
+      ('You need to be logged in!');
+    },
+    removeProduct: async (parent, { productId }, context) => {
+      if (context.user) {
+        const propduct = await Product.findOneAndDelete({
+          _id: postId,
+          author: context.user.name,
+        });
+
+        await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { products: product._id } }
+        );
+
+        return product;
+      }
+      throw AuthenticationError;
+    },
+    addPostComment: async (parent, { postId, commentText }, context) => {
+      if (context.user) {
+        return Post.findOneAndUpdate(
+          { _id: postId },
+          {
+            $addToSet: {
+              comments: { commentText, commentAuthor: context.user.name },
+            },
+          },
+          {
+            new: true,
+            runValidators: true,
+          }
+        );
+      }
+      throw AuthenticationError;
+    },
+    addProductComment: async (parent, { productId, commentText }, context) => {
+      if (context.user) {
+        return Product.findOneAndUpdate(
+          { _id: productId },
+          {
+            $addToSet: {
+              comments: { commentText, commentAuthor: context.user.name },
+            },
+          },
+          {
+            new: true,
+            runValidators: true,
+          }
+        );
+      }
+      throw AuthenticationError;
+    },
+    removePost: async (parent, { postId }, context) => {
+      if (context.user) {
+        const post = await Post.findOneAndDelete({
+          _id: postId,
+          author: context.user.name,
+        });
+
+        await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { posts: post._id } }
+        );
+
+        return post;
+      }
+      throw AuthenticationError;
+    },
+    removePostComment: async (parent, { postId, commentId }, context) => {
+      if (context.user) {
+        return Post.findOneAndUpdate(
+          { _id: postId },
+          {
+            $pull: {
+              comments: {
+                _id: commentId,
+                commentAuthor: context.user.name,
+              },
+            },
+          },
+          { new: true }
+        );
+      }
+      throw AuthenticationError;
+    },  
+    removeProductComment: async (parent, { productId, commentId }, context) => {
+      if (context.user) {
+        return Product.findOneAndUpdate(
+          { _id: productId },
+          {
+            $pull: {
+              comments: {
+                _id: commentId,
+                commentAuthor: context.user.name,
+              },
+            },
+          },
+          { new: true }
+        );
+      }
+      throw AuthenticationError;
+    },
+}
 };
+
 
 module.exports = resolvers;
