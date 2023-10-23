718d5d70264e9cccb9f2b787212f51b4d6aecd09
AyhaamB <ayh132@gmail.com>
2023-10-23 14:07:15 -0400

    working on homepage components

--- client/src/componets/HomepageComponets/DailyPost.jsx
+++ client/src/componets/HomepageComponets/DailyPost.jsx
@@ -1,6 +1,18 @@
 export default function DailyPost() {
-    return (
-      <>
-      </>
-    );
-  }
+  return (
+    <>
+      <div className="dailypost-main">
+        <div className="top-news-heading">
+          <h2 className="top-news-title">The Gran Prix is Here!</h2>
+        </div>
+        <div className="top-news-desc">
+          <p>
+            The best racers around the world come together to battle it out in
+            the newest edition of the world gran prix. Stay in the loop with all
+            the latest updates here!
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
