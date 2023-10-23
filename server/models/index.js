483f5df346d9472efb4d262af0ceddb89b0f3b7e
LeopoldoGurgel <leopoldogbp@gmail.com>
2023-10-23 12:53:00 -0400

    backEnd corrections. Sponsors included to mutations

--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,6 @@
 const User = require("./User");
 const Post = require("./Post.js");
 const Product = require("./Product");
+const Sponsor = require("./Sponsor")
 
-module.exports = {User, Post, Product};
+module.exports = {User, Sponsor, Post, Product};
