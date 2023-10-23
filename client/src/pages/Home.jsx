ea692bff65a30d370c92099c61d38dbc63e35135
Dheeraj Thind <dheerajthind@gmail.com>
2023-10-23 14:44:39 -0400

    added axios

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,21 +1,33 @@
 import { Link } from "react-router-dom";
 import { useQuery } from "@apollo/client";
 import {QUERY_USER } from "../utils/queries";
+import axios from 'axios'
+import { useEffect, useState } from 'react';
 
 //componets 
 import FavoriteRacer from "../componets/HomepageComponets/FavoriteRacer";
 
-const Home = () => {
-  const { loading, data } = useQuery(QUERY_USER, {
-    fetchPolicy: "no-cache",
-  });
 
-  const matchupList = data?.user || [];
+
+
+const Home = () => {
+ 
+  const [news, setNews] = useState()
+
+  useEffect(() =>{
+  axios.get('https://newsapi.org/v2/everything?q=F1&apiKey=66637cfa720a42078121b0c9715aed47')
+  .then((res) =>{
+    setNews(res.data)
+    console.log(news)
+  })},[])
+
+
 
   return (
     <div className="main-home">
       <div className="daily-post">
         <p>Top Post</p>
+       
       </div>
       <div className="fav-racer">
 
