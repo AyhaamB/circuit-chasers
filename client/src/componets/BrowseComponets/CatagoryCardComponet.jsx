01ed522ba6652149a3771ffeee3eb920f335389c
Dheeraj Thind <dheerajthind@gmail.com>
2023-10-24 10:25:33 -0400

    some fixes

--- client/src/componets/BrowseComponets/CatagoryCardComponet.jsx
+++ client/src/componets/BrowseComponets/CatagoryCardComponet.jsx
@@ -2,9 +2,15 @@ export default function CatagoryCard(props) {
     return (
       <>
         <div className="browse-post">
+          <div>
           <p>{props.title}</p>
+          </div>
+          <div>
           <img src={props.image}></img>
+          </div>
+          <div>
           <p>{props.desc}</p>
+          </div>
         </div>
       </>
     );
