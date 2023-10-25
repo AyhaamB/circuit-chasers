19341fd020e58045c87af7ebb3417a5a50f7c08f
Dheeraj Thind <dheerajthind@gmail.com>
2023-10-25 11:55:18 -0400

    deplooyment

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/circuit-chasers');
-
+//JPYHO91uHZbIrRoi
+//mongodb+srv://Dheeraj:JPYHO91uHZbIrRoi@cluster0.kmlkcyb.mongodb.net/?retryWrites=true&w=majority
+console.log(process.env.MONGODB_URI)
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/circuit-chasers');
+mongoose.connect('mongodb+srv://Dheeraj:JPYHO91uHZbIrRoi@cluster0.kmlkcyb.mongodb.net/circuit-chasers?retryWrites=true&w=majority');
 module.exports = mongoose.connection;
